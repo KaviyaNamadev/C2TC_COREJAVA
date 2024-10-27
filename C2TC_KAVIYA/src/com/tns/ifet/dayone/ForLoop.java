f95400ff4519d1a34f49e12c1fe0afc4af03d57b
2901k <2901k@Kaviya_04>
2024-10-27 20:29:07 +0530

    commit

--- C2TC_KAVIYA/src/com/tns/ifet/dayone/ForLoop.java
+++ C2TC_KAVIYA/src/com/tns/ifet/dayone/ForLoop.java
@@ -1,15 +1,13 @@
 package com.tns.ifet.dayone;
 
-public class Break {
+public class ForLoop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int i = 5; i < 10; i++) { 
-			   if (i == 5) 
-			    break;
+		for(int i = 1; i<=1000 ; i++) 
+			  {  
+			   System.out.print("Value of i:"); 
 			   System.out.println(i); 
-			  }
-
-	}
-
-}
+			  } 
+			 } 
+			} 
