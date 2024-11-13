36294eab17383237f6e3f974fae6aa9e2c768473
2901k <2901k@Kaviya_04>
2024-11-13 06:49:29 +0530

    commit

--- C2TC_KAVIYA/src/com/tnsif/dayten/Main.java
+++ C2TC_KAVIYA/src/com/tnsif/dayten/Main.java
@@ -3,7 +3,6 @@ public class Main {
 	public static void main(String[] args) {
 		Vehicle car = new Car();
         Vehicle bicycle = new Bicycle();
-        // Demonstrating polymorphism by calling move() on each vehicle
         car.move();       // Calls the move() method of Car
         bicycle.move();   // Calls the move() method of Bicycle
     }
--- C2TC_KAVIYA/src/com/tnsif/dayten/Vehicle.java
+++ C2TC_KAVIYA/src/com/tnsif/dayten/Vehicle.java
@@ -1,5 +1,4 @@
 package com.tnsif.dayten;
-
 public class Vehicle {
 	void move() {
         System.out.println("Vehicle is moving");
