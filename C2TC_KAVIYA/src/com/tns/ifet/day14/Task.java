fb5e4b9c573e00bb0342922661197fd14034848c
2901k <2901k@Kaviya_04>
2024-11-14 10:10:04 +0530

    commit

---
+++ C2TC_KAVIYA/src/com/tns/ifet/day14/Task.java
@@ -0,0 +1,23 @@
+package com.tns.ifet.day14;
+import java.util.PriorityQueue;
+public class Task implements Comparable<Task>{
+	private String name;
+    private int priority;
+    public Task(String name, int priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getPriority() {
+        return priority;
+    }
+    @Override
+    public int compareTo(Task other) {
+        // Higher priority tasks will have lower values
+        return Integer.compare(other.getPriority(), this.priority);
+    }
+}
+
+
